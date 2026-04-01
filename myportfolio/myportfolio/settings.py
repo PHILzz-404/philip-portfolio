8162bac3e689afc3430c496807e0005cc9cf3175
Philip Gallo <gallophilip2005@gmail.com>
2026-04-01 13:31:21 +0800

    Final settings for PythonAnywhere hosting

--- myportfolio/myportfolio/settings.py
+++ myportfolio/myportfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-0e*ts)nw!)nyh+65u!ybj!l0$0^xs1%)w9dte4i%7b1b_kgjha
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['philipgallo05.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -54,8 +55,9 @@ ROOT_URLCONF = 'myportfolio.urls'
 
 TEMPLATES = [
     {
+
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
